50daa13109c78c9464b3fdece785b8e7355a5677
Liam <liamlandeau121@gmail.com>
2026-02-26 22:45:42 +0100

    Fix Vite et App.tsx avec HashRouter pour Render

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
+import QuoteRequest from './pages/QuoteRequest';
 
 function App() {
   return (
